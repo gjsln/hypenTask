e3ee8a9113216ca6d676d85f26194b55c553843d
Gnanavel N <42657202+gjsln@users.noreply.github.com>
2019-08-02 13:33:45 +0530

    Update questionaire.tsx

--- src/components/questionaire.tsx
+++ src/components/questionaire.tsx
@@ -1,19 +1,64 @@
 import * as React from 'react';
 import '../css/site.css';
+import { CreateEditQuestionaire } from './createEditQuestionaire';
 
-export const Questionaire: React.StatelessComponent<{}> = () => {
-  return (
-    <div className='row questionaire col-8'>
-      <div className='left-head'>
+
+const answerArray:Array<any> = [
+  {
+    id:'k1',
+    candidateId:'d4545',
+    assignment:'React Test'
+  },
+  {
+    id:'k2',
+    candidateId:'d5458',
+    assignment:'Angular Test'
+  },
+  {
+    id:'k3',
+    candidateId:'d5858',
+    assignment:'Full Stack Test'
+  }
+]
+
+export class Questionaire extends React.Component {
+  state = {
+    data: answerArray
+  };
+
+  render() {
+    const items = this.state.data.map((item) =>
+    <div  className="questionaire-list col-12" >
+      <div>{item.assignment}</div>
+      <div><h6>Custom: {item.candidateId}</h6></div>
+      </div>
+    );
+    return (
+    <div className='row questionaire col-12'>
+
+      <div className='left-head col-6'>
         <strong>Question</strong>
       </div>
-      <div className='right-head'>
+
+      <div className='right-head col-6'>
         <strong>
           Question Settings&nbsp;
           <i className='fa fa-exclamation-circle' />
         </strong>
       </div>
-      <hr className='divider' />
+
+      <div className='divider' />
+      {items.length === 0 ? <div className="no-data col-12">
+        <i className="fa fa-folder"></i>
+          No Data
+      </div> :
+      <div className="col-12">
+      {items}
+      </div>}
+      <div className="col-12 space-container">
+        <CreateEditQuestionaire />
+      </div>
     </div>
   );
+    }
 };
